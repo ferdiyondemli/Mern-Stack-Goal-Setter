d0fc155f3f6b2427189138ab050f9ec1a62e7b84
ferdi yöndemli <ferdiyondemli50@gmail.com>
2022-09-21 13:48:31 +0300

    api setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+const colors=require("colors")
 const express=require("express")
 const dotenv =require("dotenv").config();
 const  errorHandler =require("./middleware/errrorMiddleware")
+const connectDB=require("./config/db")
 const port=process.env.PORT|| 5000
 const app=express();
-
+connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
